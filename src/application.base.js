299520aa3b1b184b2f689bf7069321485a248c69
Zack Davy <zdavy@fieldwire.com>
2022-03-15 12:17:07 -0500

    add promise polyfill

--- src/application.base.js
+++ src/application.base.js
@@ -2,12 +2,14 @@ window.jQuery = require('jquery');
 window.$ = window.jQuery;
 
 require('angular');
+require('bootstrap');
 require('lodash');
 require('@uirouter/angular-hybrid');
+require('angular-ui-bootstrap');
+
+window.Promise = require('../vendor/assets/javascripts/promise/promise.js');
 
 require('./load/images_loader.ts');
-
-require('angular-ui-bootstrap');
 
 require('./fieldwire.config');
 require('./fieldwire.run.ts');
